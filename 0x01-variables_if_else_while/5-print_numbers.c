34a9aa36c1c03f751ba26619d16dfebee268e76b
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-14 07:32:19 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,15 @@
 /**
  * main - Entry point
  * Return: Always 0 (Success)
- */     
+ */
 int main(void)
-{       
-        int a;
-        for (a = 0; a <= 9; a++)
-        {       
-                printf("%d",a);
-        }       
-        printf("\n");
-        return (0);
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		print("%d", a);
+	}
+	printf("\n");
+	return (0);
 }
