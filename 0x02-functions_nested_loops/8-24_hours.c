df0fac613a059c79093ad5d590a10917c3204429
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-18 08:37:31 -0700

    Jack Bauer 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b < = 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
@@ -18,11 +18,11 @@ void jack_bauer(void)
 				{
 					for (d = 0; d <= 9; d++)
 					{
-						_putchar(a + '10');
-						_putchar(b + '10');
+						_putchar(a + '0');
+						_putchar(b + '0');
 						_putchar(':');
-						_putchar(c + '10');
-						_putchar(d + '10');
+						_putchar(c + '0');
+						_putchar(d + '0');
 						_putchar('\n');
 					}
 				}
