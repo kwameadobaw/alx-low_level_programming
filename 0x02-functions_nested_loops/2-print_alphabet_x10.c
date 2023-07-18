c49f517c56e41758f34bfc8506cfc13f5d999e95
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-18 06:11:48 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet in lower case 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alpha;
+
+	while (i <= 10)
+	{
+		alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
