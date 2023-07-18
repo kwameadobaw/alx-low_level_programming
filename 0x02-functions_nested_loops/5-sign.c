fbee15ae4007e6d7fb897794f978b7b8fa8fff15
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-18 07:14:14 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Prints the corresponding sign of the value
+ *
+ * @n : The number to be checked
+ *
+ * Return: 1 if the number is positive or -1 if the number is negative or otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
