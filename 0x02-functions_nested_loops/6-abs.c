9f46e31ac0c8d4867f895d6e61d2cfbfaaa58c04
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-18 07:57:37 -0700

    Absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _abs - Prints the absolute value of an integer
- * 
+ *
  * @a: variable to check
  *
  * Return: Return the absolute value
@@ -11,6 +11,7 @@ int _abs(int a)
 	if (a < 0)
 	{
 		int b;
+
 		b = a * -1;
 
 		return (b);
