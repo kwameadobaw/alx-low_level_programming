9ea31992de4e170a6c3e7c4c513fe13983ce77ee
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-18 08:47:10 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - Prints out the times table.
+ *
+ * Return: No return value since function is void.
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+
+			_putchar(product + '0');
+			_putchar('\n');
+		}
+	}
+}
