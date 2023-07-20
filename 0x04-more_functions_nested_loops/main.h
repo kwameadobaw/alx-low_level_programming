58dad53edc079ede8df48a0b9cefd21478fc1d11
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-20 10:10:52 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +15,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+
+#endif /* MAIN_H */
