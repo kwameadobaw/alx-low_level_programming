a5e8127193f24377a5fb64bb6291a0bef3edd0ce
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-20 07:14:48 -0700

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_square - Prints square
+ *
+ * @size: The size of the square
+ *
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int a = 0;
+	int b = 0;
+
+	if (size > 0)
+	{
+		while (a <= size)
+		{
+			while (b <= size)
+			{
+				_putchar('#');
+				b++;
+			}
+			b = 0;
+			a++;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n')
+	}
+}
