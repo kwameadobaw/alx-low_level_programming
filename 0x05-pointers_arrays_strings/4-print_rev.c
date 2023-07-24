d28eef65360496db5edb27a9982c3d3ddf219d4e
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-24 09:17:19 -0700

    Print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,23 @@
-#include 
+#include "main.h"
+/**
+ * print_rev - Print a string in reverse
+ *
+ * @s: The sstring to be printed
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != 0)
+	{
+		length++;
+	}
+	for (i = length - 1; i >= 1; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
