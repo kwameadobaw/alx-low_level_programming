ca951effcd8801014969b6d04a2c0f4eb5314b2e
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-26 11:37:20 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * cap_string - Used to capitalize words of a string
+ *
+ * @str: The input string
+ *
+ * Return: Return the new string
+ */
+char *cap_string(char *str)
+{
+	int x;
+	int y;
+	int trigger;
+	char separator[] = ",;.!?(){}\n\t\" ";
+
+	for (x = 0, trigger = 0; str[x] != '\0'; x++)
+	{
+		if (str[0] >= 'a' && str[0] <= 'z')
+			trigger = 1;
+	}
+	for (y = 0; separator[y] != '\0'; y++)
+	{
+		if (separator[y] == str[x])
+			trigger = 1;
+	}
+	if (trigger == 1)
+	{
+		if (str[x] >= 'a' && str[x] <= 'z')
+			str[x] = str[x] - 32;
+			trigger = 0;
+		else if (str[x] >= 'A' && str[x] <= 'Z')
+			trigger = 0;
+		else if(str[x] >= '0' && str[x] <= '9')
+			trigger = 0;
+	}
+	return (str);
+}
