7d0b80fa89e67e5aa8787fabaddc5cb8709e9ae5
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-07-27 17:21:14 -0700

    Adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,57 @@
 #include "main.h"
 /**
- * infinite_add - Used to add integers
- * @n1: points to the first input
- * @n2: points to the second input
- * @r: points to the buffer
- * @size_r: The size of the buffer
- * Return: Always 0
+ * infinite_add - add 2 strings.
+ * @n1: string1.
+ * @n2: string2.
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: String with all letters in ROT13 base.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0;
-	int idx1 = len1 - 1;
-	int idx2 = len2 - 1;
-	int result_idx = 0;
-	int digit_sum;
-	int remainder;
-	int i;
-	char temp;
+	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
 
-	if (len1 > size_r - 1 || len2 > size_r - 1)
-	{
+	while (n1[a_len] != '\0')
+		a_len++;
+	while (n2[b_len] != '\0')
+		b_len++;
+	if (a_len > b_len)
+		biggest = a_len;
+	else
+		biggest = b_len;
+	if ((biggest + 1) >= size_r)
 		return (0);
-	}
-	while (idx1 >= 0 || idx2 >= 0 || carry)
+	r[biggest + 1] = '\0';
+
+	while (biggest >= 0)
 	{
-		digit_sum = carry;
-		if (idx1 >= 0)
+		a = (n1[a_len - 1] - '0');
+		b = (n1[b_len - 1] - '0');
+		if (a_len > 0 && b_len > 0)
+			sum = a + b + carry;
+		else if (a_len < 0 && b_len > 0)
+			sum = b + carry;
+		else if (a_len > 0 && b_len < 0)
+			sum = a + carry;
+		else
+			sum = carry;
+		if (sum > 9)
 		{
-			digit_sum += n1[idx1] - '0';
-			idx2--;
+			carry = sum / 10;
+			sum = (sum % 10) + '0';
 		}
-		carry = digit_sum / 10;
-		remainder = digit_sum % 10;
-		if (result_idx >= size_r - 1)
+		else
 		{
-			return (0);
+			carry = 0;
+			sum = sum + '0';
 		}
-		r[result_idx] = remainder + '0';
-		result_idx++;
+		r[biggest] = sum;
+		a_len--;
+		b_len--;
+		biggest--;
 	}
-	r[result_idx] = '\0';
-	for (i = 0, j = result_idx - 1; i < j; i++, j--)
-	{
-		temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	}
-	return (r);
+	if (*(r) != 0)
+		return (r);
+	else
+		return (r + 1);
 }
