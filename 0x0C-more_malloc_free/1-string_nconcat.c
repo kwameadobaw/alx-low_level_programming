22927f89e5246056add6de2594ec5f70446851fb
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-08-10 10:06:51 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,37 +11,20 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int length_s1 = 0;
-	unsigned int length_s2 = 0;
+	unsigned int s1_len = strlen(s1);
+	unsigned int s2_len = strlen(s2);
 	char *result;
-	unsigned int a;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (a = 0; s1[a] != '\0'; a++)
-		length_s1++;
-	for (a = 0; s2[a] != '\0'; a++)
-		length_s2++;
-	result = malloc((length_s1 + n) * sizeof(char) + 1);
+	if (n >= s2_len)
+		n = s2_len;
+	result = (char *)malloc(s1_len + n + 1);
 	if (result == NULL)
 		return (NULL);
-	if (n >= length_s2)
-	{
-		for (a = 0; s1[a] != '\0'; a++)
-			result[a] = s1[a];
-		for (a = 0; s2[a] != '\0'; a++)
-			result[length_s1 + a] = s2[a];
-		result[length_s1] = '\0';
-	}
-	else
-	{
-		for (a = 0; s1[a] != '\0'; a++)
-			result[a] = s1[a];
-		for (a = 0; a < n; a++)
-			result[length_s1 + a] = s2[a];
-		result[length_s1 + a] = '\0';
-	}
+	strcpy(result, s1);
+	strncat(result, s2, n);
 	return (result);
 }
