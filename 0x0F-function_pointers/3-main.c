8ba395af1b1b5655db12745ab25699a1002590e8
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-08-16 07:21:43 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - entry point
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int number_1, number_2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	number_1 = atoi(argv[1]);
+	number_2 = atoi(argv[3]);
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (number_2 == 0 && ((*argv[2]) == '/' || (*argv[2]) == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", (get_op_func(argv[2])(number_1, number_2)));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds 2 numbers
+ * @a: first variable
+ * @b: Second variable
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtructs 2 numbers
+ * @a: first variable
+ * @b: second variable
+ * Return: the difference of 2 numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mulptiplies 2 numbers
+ * @a: first variable
+ * @b: second variable
+ * Return: The multiplication of 2 numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides 2 numbers
+ * @a: First variable
+ * @b: Second variable
+ * Return: the division of 2 numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder after division
+ * @a: first variable
+ * @b: second variable
+ * Return: The remainder after division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
