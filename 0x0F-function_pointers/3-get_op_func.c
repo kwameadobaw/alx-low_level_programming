0e43901a96b733e8fb4cb3ed82043e5751321e78
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-08-16 07:30:58 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,18 +1,19 @@
 #include "3-calc.h"
 /**
- * get_op_func: pointer to a function
+ * get_op_func - pointer to a function
  * @s: choice of the user
+ * Return: Result
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	       	{"+", op_add},
-	       	{"-", op_sub},
+		{"+", op_add},
+		{"-", op_sub},
 		{"*", op_mul},
-	       	{"/", op_div},
-	       	{"%", op_mod},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
-    	};
+	};
 	int i = 0;
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
