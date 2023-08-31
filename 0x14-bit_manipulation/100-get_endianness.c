036c53dbe171e9a7e7d5909a059eb269f4d32e00
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-08-31 20:48:05 +0000

    a function that checks the endianness.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * get_endianness - returns processor endianness
+  * Return: 0 if big endian, 1 if little endian
+  */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	unsigned char *byteptr = (unsigned char *)&num;
+
+	return (*byteprt == 1);
+}
