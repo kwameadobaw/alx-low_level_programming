62b36e4695e3abf2c5c606ce05941ded86ad6c83
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-10-16 13:49:17 +0000

    a dynamic library that contains C functions that can be called from Python. See example for more detail.

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,73 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *
+ * @a: variable to be added
+ *
+ * @b: variable to be added
+ * Return: Return the sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mul - check the code.
+ *
+ * @a: First number
+ *
+ * @b: Second number
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
+
+/**
+ * sub - check the code.
+ *
+ * @a: First number
+ *
+ * @b: Second number
+ *
+ * Return: Always 0.
+ */
+int sub(int a, int b)
+{
+
+	return (a - b);
+}
+
+/**
+ * div - check the code.
+ *
+ * @a: First number
+ *
+ * @b: Second number
+ *
+ * Return: Always 0.
+ */
+int div(int a, int b)
+{
+
+	return (a / b);
+}
+
+/**
+ * mod - check the code.
+ *
+ * @a: First number
+ *
+ * @b: Second number
+ *
+ * Return: Always 0.
+ */
+int mod(int a, int b)
+{
+
+	return (a % b);
+}
